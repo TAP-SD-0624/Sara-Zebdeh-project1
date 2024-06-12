39d7372f2648dbbd5456f59b39e581ad94f64d9d
Sara <sarazebdeh73@gmail.com>
2024-06-12 19:45:20 +0300

    css changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const favoriteBtn = document.getElementById("favourites-click-btn");
 const favouritesDiv = document.querySelector(".favourites");
 
 const modeText = document.getElementById("mode_btn_text");
+const heartColor = document.getElementById("heart-icon-btn");
+const modeColor = document.getElementById("mode-icon-btn");
 
 colorThemeBtn.addEventListener("click", () => {
   body.classList.contains("dark")
@@ -22,4 +24,12 @@ colorThemeBtn.addEventListener("click", () => {
 
 favoriteBtn.addEventListener("click", () => {
   favouritesDiv.classList.toggle("show");
+});
+
+favoriteBtn.addEventListener('click', function() {
+  heartColor.classList.toggle('clicked');
+});
+
+colorThemeBtn.addEventListener('click', function() {
+  modeColor.classList.toggle('clicked');
 });
